1cc622fcb00e53f16608dd3e9a7b89330589654b
Mingcai SHEN <archsh@gmail.com>
2013-06-19 02:08:41 +0800

    Updated.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # pinyin-python
 
 A simple pinyin engine for Python.
+
+Usage:
+
+>>> import pinyin
+>>> py = Pinyin() # You can initialize with your words lib in UTF-8 text format.
+>>> for w in py.query('tian'): print w
--- pinyin/pyin.py
+++ pinyin/pyin.py
@@ -7,20 +7,56 @@ def code2unichar(code):
 
 class Pinyin(object):
     #code
-    def __init__(self, filename=None):
+    def __init__(self, *filename):
         """
-        Initialize a Pinyin instance. filename for init with a user lib filename.
+        Initialize a Pinyin instance. filename for init with a user word lib filename.
+        Use a list if you need multiple filename.
         """
         from data import PINYIN_WORDS
-        self._pydata=PINYIN_WORDS
+        self._pydata=[list(row) for row in PINYIN_WORDS]
+        self._wordlib={}
+        if not filename:
+            filename = [
+                os.path.join(os.path.dirname(os.path.abspath(__file__)),'wordlib0.txt'),
+                os.path.join(os.path.dirname(os.path.abspath(__file__)),'wordlib1.txt'),
+            ]
         if filename is not None:
             self.load(filename)
     
     def load(self, filename):
-        '''
+        """
         Load user lib data file.
-        '''
-        pass
+        """
+        def matching(x,y=None):
+            if not isinstance(x,dict):
+                k = x
+                x = self._wordlib
+                if k in x: x[k]+=1
+                else: x[k]=1
+            if y:
+                if y in x: x[y]+=1
+                else: x[y]=1
+            return x
+        
+        if isinstance(filename,file):
+            content = filename.read().decode('utf8').strip()
+            #print 'length:',len(content)
+            if len(content)>10:
+                self._wordlib = reduce(matching,list(content))
+            new_data = list()
+            for row in self._pydata:
+                words = row[1:]
+                words.sort(key=lambda x:self._wordlib.get(unicode(x,'utf8'),0),reverse=True)
+                row = row[:1]+words #row[1:].sort(key=lambda x:self._wordlib.get(x,0))
+                new_data.append(row)
+            self._pydata = new_data
+        elif isinstance(filename,(tuple,list)):
+            for fname in filename:
+                self.load(fname)
+        else:
+            f = open(filename)
+            self.load(f)
+
     
     def query(self, py):
         import datetime
@@ -31,12 +67,12 @@ class Pinyin(object):
             return False
         def ext(x,y=None):
             if not isinstance(x,list):
-                x = list(x[1:])
+                x = list(x)
             if y:
-                x.extend(y[1:])
+                x.extend(y)
             return x
         #t1 = datetime.datetime.now()
-        result = filter(match_pinyin,self._pydata)
+        result = map(lambda x: x[1:],filter(match_pinyin,self._pydata))
         #print result
         #t2 = datetime.datetime.now()
         #sorted(filter(lambda x: x is not None,map(match_pinyin, self._pydata)),key=lambda x: x[0])
