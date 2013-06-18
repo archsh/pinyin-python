98830c16a84c169fc638dd198feafe9ce61931aa
Mingcai SHEN <archsh@gmail.com>
2013-06-18 23:54:41 +0800

    Improved performance.

--- pinyin/pyin.py
+++ pinyin/pyin.py
@@ -11,12 +11,14 @@ class Pinyin(object):
     def __init__(self, filename=None):
         #if filename is None:
         #    filename = os.path.join(os.path.abspath(os.path.dirname(__file__)),'pinyin.dat')
-        self._pydata=list()
-        self.load(filename)
+        from data import PINYIN_WORDS
+        self._pydata=PINYIN_WORDS
+        #self.load(filename)
     
     def load(self, filename=None):
+        return
         if filename is None:
-            from .pydata import PINYIN_WORDS
+            from data import PINYIN_WORDS
             self._pydata = PINYIN_WORDS
         else:
             for line in open(filename):
@@ -28,13 +30,28 @@ class Pinyin(object):
                 self._pydata.append(row)
     
     def query(self, py):
+        import datetime
+        py = py.upper()
         def match_pinyin(x):
-            for w in x[1:]:
-                if w.lower()[:len(py)]==py.lower():
-                    return (w,x[0])
-            return None
-        result = sorted(filter(lambda x: x is not None,map(match_pinyin, self._pydata)),key=lambda x: x[0])
-        return map(lambda x:x[1],result)
+            if x[0][:len(py)]==py:
+                return True
+            return False
+        def ext(x,y):
+            if not isinstance(x,list):
+                x = list(x[1:])
+            if y:
+                x.extend(y[1:])
+            return x
+        
+        #t1 = datetime.datetime.now()
+        result = filter(match_pinyin,self._pydata)
+        #t2 = datetime.datetime.now()
+        #sorted(filter(lambda x: x is not None,map(match_pinyin, self._pydata)),key=lambda x: x[0])
+        result = reduce(ext,result)
+        #t3 = datetime.datetime.now()
+        #print 'query> t2-t1:',t2-t1
+        #print 'query> t3-t2:',t3-t2
+        return result
         #return map(lambda x: x[0], filter(match_pinyin, self._pydata))
             
         
@@ -50,4 +67,52 @@ class Pinyin(object):
             f.write(''.join(line))
         f.write(')\n')
         f.close()
+
+if __name__ == '__main__':
+    import sys, datetime
+    t1 = datetime.datetime.now()
+    py = Pinyin()
+    t2 = datetime.datetime.now()
+    ret = py.query('ch')
+    t3 = datetime.datetime.now()
+    print len(ret)
+    
+    for x in ret:
+        print x,
+    print ''
+    print 't2-t1:',t2-t1
+    print 't3-t2:',t3-t2
+    
+
+if __name__ == '__TmainT__':
+    import sys, datetime
+    from pydata import PINYIN_WORDS
+    WORDS={}
+    for row in PINYIN_WORDS:
+        py = row[0][:-1]
+        if py in WORDS:
+            WORDS[py].append(row[1])
+        else:
+            WORDS[py]=[row[1]]
+    if len(sys.argv)>1:
+        f = open(sys.argv[1],'w')
+        lines = list()
+        for k,v in WORDS.items():
+            lines.append([k]+v)
+        lines.sort(key=lambda x:x[0])
+        f.write('# -*- coding:utf-8 -*-\n')
+        f.write('PINYIN_WORDS=(\n')
+        for row in lines:
+            line=['\t']
+            line.append('(')
+            line.append(','.join(['"%s"'%x for x in row]))
+            line.append('),\n')
+            f.write(''.join(line))
+        f.write(')\n')
+        f.close()
+    else:
+        for k,v in WORDS.items():
+            print k,':',len(v)  
+    
+    
     
--- test/test_pinyin.py
+++ test/test_pinyin.py
@@ -2,7 +2,7 @@
 
 import sys
 sys.path.append('../')
-
+import datetime
 import pinyin
 import unittest
 
@@ -22,8 +22,14 @@ class DefaultTestCase(unittest.TestCase):
         self.assertGreater(len(py._pydata),20000)
     
     def test_query(self):
+        t1 = datetime.datetime.now()
         py = pinyin.Pinyin()
-        for w in py.query('duan'): print w
+        t2 = datetime.datetime.now()
+        ret = py.query('d')
+        t3 = datetime.datetime.now()
+        #print ret
+        print 't2-t1:',t2-t1
+        print 't3-t2:',t3-t2
         #for w in py.query('ti'): print w
         #for w in py.query('bei'): print w
         #for w in py.query('da'): print w
