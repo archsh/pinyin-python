1df67936138382035a0ac494dc0d4619b7e258c2
Mingcai SHEN <archsh@gmail.com>
2013-07-12 05:47:02 +0800

    Updated: phrase loaded.

--- pinyin/gen_data.py
+++ pinyin/gen_data.py
@@ -94,23 +94,100 @@ def write_json(filename,datas):
     if to_close:
         filename.close()
 
+from raw_data import PINYIN_WORDS
+PINYINS = dict([(x[1],x[0].lower()) for x in PINYIN_WORDS])
+def get_pinyin(word):
+    if word in PINYINS:
+        return PINYINS[word]
+    return None
+
+def is_zh(c):
+    x = ord (c)
+    # Punct & Radicals
+    if x >= 0x2e80 and x <= 0x33ff:
+        return True
+    # Fullwidth Latin Characters
+    elif x >= 0xff00 and x <= 0xffef:
+        return True
+    # CJK Unified Ideographs &
+    # CJK Unified Ideographs Extension A
+    elif x >= 0x4e00 and x <= 0x9fbb:
+        return True
+    # CJK Compatibility Ideographs
+    elif x >= 0xf900 and x <= 0xfad9:
+        return True
+    # CJK Unified Ideographs Extension B
+    elif x >= 0x20000 and x <= 0x2a6d6:
+        return True
+    # CJK Compatibility Supplement
+    elif x >= 0x2f800 and x <= 0x2fa1d:
+        return True
+    else:
+        return False
+
+def process_phrase(phrase):
+    assert isinstance(phrase,unicode)
+    output=list()
+    cur_zh=None
+    cur_py=None
+    for c in phrase:
+        if not is_zh(c):
+            if not cur_py:
+                cur_py = c
+            else:
+                cur_py += c
+        else:
+            if cur_zh:
+                #print 'Get:', cur_zh, 'Py:',cur_py
+                output.append((cur_zh, cur_py if cur_py else get_pinyin(cur_zh)))
+                cur_py = None
+            cur_zh = c
+    else:
+        if cur_zh:
+            #print 'Get:', cur_zh, 'Py:',cur_py
+            output.append((cur_zh, cur_py if cur_py else get_pinyin(cur_zh)))
+            cur_py = None
+    return output
+    
+    
+
+
 def load_txt_phrases(filename):
-    pass
+    to_close = False
+    if isinstance(filename,(str,unicode)):
+        to_close = True
+        filename = open(filename,'rU')
+    if not isinstance(filename,file):
+        raise Exception('Not a file handle.')
+    datas = dict()
+    for line in filename.readlines():
+        line = unicode(line.strip(),'utf8')
+        if not line: continue
+        #print 'line:>',line
+        word,phrases = line.split(u' ',1)
+        phrases = [process_phrase(x) for x in filter(lambda x:x, phrases.split(','))]
+        if len(word)>1:
+            py = word[1:]
+        else:
+            py = get_pinyin(word[0])
+        #print word[0],'(%s)'%py,':',phrases
+        if py in datas:
+            datas[py].append((word[0],phrases))
+        else:
+            datas[py]=[(word[0],phrases)]
+        #datas.append(((word[0],py),phrases))
+    if to_close:
+        filename.close()
+    return datas
 
 
-if __name__ == '__main__':
+if __name__ == '__!!!!!main__': ### For Dictionary
     import sys, datetime
-    from data import PINYIN_WORDS
     if len(sys.argv)>1:
         t1 = datetime.datetime.now()
         dictionary = load_txt_dictionary(sys.argv[1])
         t2 = datetime.datetime.now()
         if len(sys.argv)>2:
-            #f = open(sys.argv[2],'w')
-            #for py,words in dictionary:
-            #    line = u'%s %s\n'%(py, u''.join([x[0] for x in words]))
-            #    f.write(line.encode('utf8'))
-            #f.close()
             #write_data_python(sys.argv[2],dictionary)
             write_json(sys.argv[2],dictionary)
             t3 = datetime.datetime.now()
@@ -134,4 +211,37 @@ if __name__ == '__main__':
     else:
         print 'Please enter the dictionary filename.'
         
-
+if __name__ == '__main__': ### For Phrases
+    import sys, datetime
+    if len(sys.argv)>1:
+        t1 = datetime.datetime.now()
+        dictionary = load_txt_phrases(sys.argv[1])
+        t2 = datetime.datetime.now()
+        if len(sys.argv)>2:
+            #f = open(sys.argv[2],'w')
+            #for py,words in dictionary:
+            #    line = u'%s %s\n'%(py, u''.join([x[0] for x in words]))
+            #    f.write(line.encode('utf8'))
+            #f.close()
+            #write_data_python(sys.argv[2],dictionary)
+            write_json(sys.argv[2],dictionary)
+            t3 = datetime.datetime.now()
+            dictionary = load_json(filename=sys.argv[2])
+            t4 = datetime.datetime.now()
+            print 't2-t1:', t2-t1
+            print 't3-t2:', t3-t2
+            print 't4-t3:', t4-t3
+            #for py,words in dictionary:
+            #    words = sorted(words,key=lambda x:x[1])
+            #    print py,':',
+            #    for w,n in words:
+            #        print w,'(%d)'%n,
+            #    print '\n'
+        else:
+            pass
+            #print json.dumps(dictionary,indent=' ')
+            
+            #write_json(sys.stdout,dictionary)
+        
+    else:
+        print 'Please enter the dictionary filename.'
